bded18d6fd6b063a72923c6ea6abc15b77430bc6
Ehsan Hasin <ehsanhasin@gmail.com>
2024-11-14 10:59:27 +0430

    initial adding pages and dart files

---
+++ lib/utilities/my_constants.dart
@@ -0,0 +1,14 @@
+//app name
+import 'package:flutter/material.dart';
+
+final String APP_Name = "لینکستان";
+final String APP_VERSION = "۰.۰.۱";
+
+//rout names
+final String ROUTE_SPLASH_SCREEN_PAGE = "Splash";
+final String ROUTE_HOME_PAGE = "Home";
+final String ROUTE_SETTING_PAGE = "Setting";
+
+
+//Styles
+myTextStyle() => TextStyle(fontSize: 30);
